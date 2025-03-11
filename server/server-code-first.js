76c9e5a3ed596b0ddae17d0410d5285683c77632
Oleksandr <oslavych@noltic.com>
2025-03-11 16:46:26 +0200

    add 'other' case to definition resolvers

--- server/server-code-first.js
+++ server/server-code-first.js
@@ -7,6 +7,9 @@ const Query = queryType({
         t.string('greeting', {
             resolve: () => 'Hello World!',
         });
+        t.string('other', {
+            resolve: () => 'Hi there!',
+        })
     }
 })
 
