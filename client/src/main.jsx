21cf626d3ad76cd2731a7aab824f443065896037
Oleksandr <oslavych@noltic.com>
2025-03-12 12:26:55 +0200

    Add required components to app. Stylize app

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,5 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import 'bulma/css/bulma.css';
 import App from './App'
 
 createRoot(document.getElementById('root')).render(
