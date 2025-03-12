4754f02974a0e9fea71b390243c9dc6c4cc80f3e
Oleksandr <oslavych@noltic.com>
2025-03-12 15:25:38 +0200

    associate each job with a company. Did query more functional

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,8 +1,19 @@
 import { getJobs } from './db/jobs.js';
+import { getCompany } from './db/companies.js';
 
 export const resolvers = {
     Query: {
         greeting: () => 'Hello World!',
         jobs: () => getJobs()
     },
+
+    Job: {
+        date: (job) => formatDate(job.createdAt),
+        company: (job) => getCompany(job.companyId)
+    }
+}
+
+function formatDate(dateToFormat) {
+    return new Date(dateToFormat)
+    .toLocaleDateString("en-GB") // 27/01/2025
 }
