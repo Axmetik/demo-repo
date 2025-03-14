fc4c4092beb62c9b921b5688bbb648d2963b54a9
Oleksandr <oslavych@noltic.com>
2025-03-14 15:24:45 +0200

    fix date field(createdAt)

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,19 +1,15 @@
-import { getJobs } from './db/jobs.js';
+import { getJob, getJobs } from './db/jobs.js';
 import { getCompany } from './db/companies.js';
 
 export const resolvers = {
     Query: {
         greeting: () => 'Hello World!',
-        jobs: () => getJobs()
+        jobs: () => getJobs(),
+        job: (_root, { id }) => getJob(id),
     },
 
     Job: {
-        // date: (job) => formatDate(job.createdAt),
+        date: (job) => job.createdAt,
         company: (job) => getCompany(job.companyId)
     }
-}
-
-function formatDate(dateToFormat) {
-    return new Date(dateToFormat)
-    .toLocaleDateString("en-GB") // 27/01/2025
 }
