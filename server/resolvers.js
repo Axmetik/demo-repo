71d35e83458b20124976c9af63086f4045d1818f
Oleksandr <oslavych@noltic.com>
2025-03-14 16:49:09 +0200

    Implement company fetching logic

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,4 +1,4 @@
-import { getJob, getJobs } from './db/jobs.js';
+import { getJob, getJobs, getJobsByCompany } from './db/jobs.js';
 import { getCompany } from './db/companies.js';
 
 export const resolvers = {
@@ -6,10 +6,15 @@ export const resolvers = {
         greeting: () => 'Hello World!',
         jobs: () => getJobs(),
         job: (_root, { id }) => getJob(id),
+        company: (_root, { id }) => getCompany(id)
     },
 
     Job: {
         date: (job) => job.createdAt,
         company: (job) => getCompany(job.companyId)
+    },
+
+    Company: {
+        jobs: (company) => getJobsByCompany(company.id)
     }
 }
