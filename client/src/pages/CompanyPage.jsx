19c5ee4163a86f1249e575ce562f84870dff5a0e
Oleksandr <oslavych@noltic.com>
2025-03-19 16:13:31 +0200

    Client side error management

--- client/src/pages/CompanyPage.jsx
+++ client/src/pages/CompanyPage.jsx
@@ -1,4 +1,4 @@
-import { Link, useParams } from 'react-router';
+import { useParams } from 'react-router';
 import { useEffect, useState } from 'react';
 import { getCompany } from '../lib/graphql/queries';
 import JobList from '../components/JobList';
@@ -6,16 +6,42 @@ import JobList from '../components/JobList';
 function CompanyPage() {
   const { companyId } = useParams();
 
-  const [company, setCompany] = useState();
+  const [state, setState] = useState({
+    company: null,
+    loading: true,
+    error: false
+  });
 
   useEffect(() => {
-      getCompany(companyId).then(cmp => setCompany(cmp));
+      (async () => {
+        try {
+          const company = await getCompany(companyId);
+          setState({
+            company,
+            loading: false,
+            error: false
+          })
+        } catch (error) {
+          setState({
+            company: null,
+            loading: false,
+            error: true
+          })
+        }
+      })();
   }, [companyId]);
 
-  console.log('[Company] cmp:', company);
-  if (!company) {
+  console.log('[Company] cmp:', state);
+
+  const {company, loading, error} = state;
+
+  if(loading) {
     return <div>Loading...</div>;
   }
+  if(error) {
+    return <h1 className="title">Company Not Found</h1>
+  }
+
   return (
     <div>
       <h1 className="title">
