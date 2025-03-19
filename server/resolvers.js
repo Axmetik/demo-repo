a9f9b7438f7e301ab52527d6290d96f2377a04df
Oleksandr <oslavych@noltic.com>
2025-03-19 15:53:43 +0200

    Custom error creation and handling

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,12 +1,29 @@
 import { getJob, getJobs, getJobsByCompany } from './db/jobs.js';
 import { getCompany } from './db/companies.js';
+import { GraphQLError } from 'graphql';
 
 export const resolvers = {
     Query: {
         greeting: () => 'Hello World!',
         jobs: () => getJobs(),
-        job: (_root, { id }) => getJob(id),
-        company: (_root, { id }) => getCompany(id)
+        job: async (_root, { id }) => {
+            const job = await getJob(id);
+
+            if(!job) {
+                throw notFoundError('No job found with id '+ id)
+            }
+                
+            return job;
+        },
+        company: async (_root, { id }) => {
+            const company = await getCompany(id);
+
+            if(!company) {
+                throw notFoundError('No company found with id '+ id)
+            }
+                
+            return company;
+        }
     },
 
     Job: {
@@ -17,4 +34,10 @@ export const resolvers = {
     Company: {
         jobs: (company) => getJobsByCompany(company.id)
     }
+}
+
+function notFoundError(message) {
+    return new GraphQLError(message, {
+        extensions: { code: 'NOT_FOUND' }
+    });
 }
